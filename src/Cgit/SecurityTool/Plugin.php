fd9070dc2d82443a9f5adec213696faa6e7ddf78
John Hughes <john@castlegateit.co.uk>
2017-05-24 09:33:25 +0100

    Restrict network warning to network sites

--- src/Cgit/SecurityTool/Plugin.php
+++ src/Cgit/SecurityTool/Plugin.php
@@ -160,7 +160,7 @@ class Plugin
      */
     private function checkNetworkCompatible()
     {
-        if (self::networkCompatible()) {
+        if (self::networkCompatible() && is_multisite()) {
             return;
         }
 
